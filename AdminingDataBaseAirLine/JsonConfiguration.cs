333b1628949ae411346462f1c2765c7cfe27c4fb
CJmean4ik <gromovstas64@gmail.com>
2023-03-09 17:53:01 +0200

    Создал и заполнил конфигурационный файл. Создал класс для считывание строки подключения

---
+++ AdminingDataBaseAirLine/JsonConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AdminingDataBaseAirLine
+{
+    public class JsonConfiguration
+    {
+
+        public async Task<string> GetConnectionString(string path)
+        {
+            using (JsonDocument jsonDocument = await JsonDocument.ParseAsync(File.Open(path, FileMode.Open)))
+            {
+               return jsonDocument.RootElement.GetProperty("MsSqlConnection").GetString()!;           
+            }
+
+        }
+    }
+}
