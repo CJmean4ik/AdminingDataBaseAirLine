8f8c63c04ed97df0fe96760809c89ef5c8060e71
CJmean4ik <gromovstas64@gmail.com>
2023-04-13 15:22:52 +0300

    Добавил Форму Airlineplane и добавил функцию для заполнения данных в Datagridview

---
+++ AdminingDataBaseAirLine/UserControls/AirlineplanePanel/AirlineplaneControl.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace AdminingDataBaseAirLine.UserControls.AirlineplanePanel
+{
+    partial class AirlineplaneControl
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.idColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.senderColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fromWhereColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.WhereColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.departmentColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.arrivalColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.InRaceColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.airplaneSender = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(126)))), ((int)(((byte)(245)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("JetBrains Mono", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(126)))), ((int)(((byte)(245)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idColumn,
+            this.senderColumn,
+            this.fromWhereColumn,
+            this.WhereColumn,
+            this.departmentColumn,
+            this.arrivalColumn,
+            this.InRaceColumn,
+            this.airplaneSender});
+            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(126)))), ((int)(((byte)(245)))));
+            dataGridViewCellStyle9.Font = new System.Drawing.Font("JetBrains Mono", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle9.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle9.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle9.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle9;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.dataGridView1.EnableHeadersVisualStyles = false;
+            this.dataGridView1.GridColor = System.Drawing.Color.White;
+            this.dataGridView1.Location = new System.Drawing.Point(24, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle10.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle10.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle10.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle10.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle10.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle10.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle10.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle10;
+            this.dataGridView1.RowHeadersVisible = false;
+            dataGridViewCellStyle11.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle11.Font = new System.Drawing.Font("JetBrains Mono", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle11.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(126)))), ((int)(((byte)(245)))));
+            dataGridViewCellStyle11.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(126)))), ((int)(((byte)(245)))));
+            dataGridViewCellStyle11.SelectionForeColor = System.Drawing.SystemColors.Window;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle11;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.ShowEditingIcon = false;
+            this.dataGridView1.Size = new System.Drawing.Size(929, 573);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(126)))), ((int)(((byte)(245)))));
+            this.panel1.Location = new System.Drawing.Point(18, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(941, 576);
+            this.panel1.TabIndex = 2;
+            // 
+            // idColumn
+            // 
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("JetBrains Mono", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(126)))), ((int)(((byte)(245)))));
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(126)))), ((int)(((byte)(245)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.Window;
+            this.idColumn.DefaultCellStyle = dataGridViewCellStyle2;
+            this.idColumn.FillWeight = 50F;
+            this.idColumn.HeaderText = "ID";
+            this.idColumn.Name = "idColumn";
+            this.idColumn.ReadOnly = true;
+            this.idColumn.Width = 50;
+            // 
+            // senderColumn
+            // 
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("JetBrains Mono", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(126)))), ((int)(((byte)(245)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(126)))), ((int)(((byte)(245)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.Window;
+            this.senderColumn.DefaultCellStyle = dataGridViewCellStyle3;
+            this.senderColumn.FillWeight = 150F;
+            this.senderColumn.HeaderText = "Авіакомпанія";
+            this.senderColumn.Name = "senderColumn";
+            this.senderColumn.ReadOnly = true;
+            this.senderColumn.Width = 150;
+            // 
+            // fromWhereColumn
+            // 
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("JetBrains Mono", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(126)))), ((int)(((byte)(245)))));
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(126)))), ((int)(((byte)(245)))));
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.Window;
+            this.fromWhereColumn.DefaultCellStyle = dataGridViewCellStyle4;
+            this.fromWhereColumn.HeaderText = "ID літака";
+            this.fromWhereColumn.Name = "fromWhereColumn";
+            this.fromWhereColumn.ReadOnly = true;
+            // 
+            // WhereColumn
+            // 
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("JetBrains Mono", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(126)))), ((int)(((byte)(245)))));
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(126)))), ((int)(((byte)(245)))));
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.Window;
+            this.WhereColumn.DefaultCellStyle = dataGridViewCellStyle5;
+            this.WhereColumn.HeaderText = "Тип літака";
+            this.WhereColumn.Name = "WhereColumn";
+            this.WhereColumn.ReadOnly = true;
+            // 
+            // departmentColumn
+            // 
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("JetBrains Mono", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(126)))), ((int)(((byte)(245)))));
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(126)))), ((int)(((byte)(245)))));
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.Window;
+            this.departmentColumn.DefaultCellStyle = dataGridViewCellStyle6;
+            this.departmentColumn.FillWeight = 170F;
+            this.departmentColumn.HeaderText = "Вмісткість";
+            this.departmentColumn.Name = "departmentColumn";
+            this.departmentColumn.ReadOnly = true;
+            // 
+            // arrivalColumn
+            // 
+            dataGridViewCellStyle7.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("JetBrains Mono", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(126)))), ((int)(((byte)(245)))));
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(126)))), ((int)(((byte)(245)))));
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.Window;
+            this.arrivalColumn.DefaultCellStyle = dataGridViewCellStyle7;
+            this.arrivalColumn.FillWeight = 170F;
+            this.arrivalColumn.HeaderText = "Модель літака";
+            this.arrivalColumn.Name = "arrivalColumn";
+            this.arrivalColumn.ReadOnly = true;
+            this.arrivalColumn.Width = 180;
+            // 
+            // InRaceColumn
+            // 
+            this.InRaceColumn.HeaderText = "В рейсі";
+            this.InRaceColumn.Name = "InRaceColumn";
+            this.InRaceColumn.ReadOnly = true;
+            // 
+            // airplaneSender
+            // 
+            dataGridViewCellStyle8.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("JetBrains Mono", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(126)))), ((int)(((byte)(245)))));
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(126)))), ((int)(((byte)(245)))));
+            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.Window;
+            this.airplaneSender.DefaultCellStyle = dataGridViewCellStyle8;
+            this.airplaneSender.FillWeight = 150F;
+            this.airplaneSender.HeaderText = "Країна ласник";
+            this.airplaneSender.Name = "airplaneSender";
+            this.airplaneSender.ReadOnly = true;
+            this.airplaneSender.Width = 150;
+            // 
+            // AirlineplaneControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "AirlineplaneControl";
+            this.Size = new System.Drawing.Size(977, 576);
+            this.Load += new System.EventHandler(this.AirlineplaneControl_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Panel panel1;
+        private DataGridViewTextBoxColumn idColumn;
+        private DataGridViewTextBoxColumn senderColumn;
+        private DataGridViewTextBoxColumn fromWhereColumn;
+        private DataGridViewTextBoxColumn WhereColumn;
+        private DataGridViewTextBoxColumn departmentColumn;
+        private DataGridViewTextBoxColumn arrivalColumn;
+        private DataGridViewTextBoxColumn InRaceColumn;
+        private DataGridViewTextBoxColumn airplaneSender;
+    }
+}
---
+++ AdminingDataBaseAirLine/UserControls/AirlineplanePanel/AirlineplaneControl.cs
@@ -0,0 +1,58 @@
+﻿using System.Data;
+
+namespace AdminingDataBaseAirLine.UserControls.AirlineplanePanel
+{
+    public partial class AirlineplaneControl : UserControl
+    {
+        private AirlineContext _airlineContext;
+
+        public AirlineplaneControl()
+        {
+            InitializeComponent();
+        }
+
+        public AirlineplaneControl(AirlineContext airlineContext) : this()
+        {
+            _airlineContext = airlineContext;
+        }
+
+        private void AirlineplaneControl_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void AddItemToDataGridView()
+        {
+            var Flights = _airlineContext.AirlinePlanes.Select(s => new
+            {
+                s.ID,
+                AirplaneId = s.Airplane.ID,
+                s.Airplane.Model,
+                s.Airplane.Capacity,
+                s.Airplane.TypeAirplane,
+                s.AirLine.Name,
+                s.AirLine.Owner,
+                s.InRaise
+            }).ToList();
+
+            int count = Flights.Count();
+            dataGridView1.Rows.Add(count);
+
+            for (int i = 0; i < count; i++)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = Flights[i].ID;
+                dataGridView1.Rows[i].Cells[1].Value = Flights[i].Name;
+                dataGridView1.Rows[i].Cells[2].Value = Flights[i].AirplaneId;
+                dataGridView1.Rows[i].Cells[3].Value = Flights[i].TypeAirplane;
+                dataGridView1.Rows[i].Cells[4].Value = Flights[i].Capacity;
+                dataGridView1.Rows[i].Cells[5].Value = Flights[i].Model;
+                dataGridView1.Rows[i].Cells[6].Value = Flights[i].InRaise;
+                dataGridView1.Rows[i].Cells[7].Value = Flights[i].Owner;
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
